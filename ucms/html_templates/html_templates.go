179eee732db279c45f74d54d87aeb84c9aa1d1f1
Jeremy Busk <jeremybusk@gmail.com>
2024-04-25 17:45:42 -0600

    fmt

--- ucms/html_templates/html_templates.go
+++ ucms/html_templates/html_templates.go
@@ -4,12 +4,12 @@ const Markdown = `
   <!DOCTYPE html>
   <html lang="en">
   <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>{{.Title}}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{.Title}}</title>
   </head>
   <body>
-         {{ .Body }}
+    {{ .Body }}
   </body>
   </html>
 `
@@ -18,19 +18,19 @@ const MDBootstrap = `
   <!DOCTYPE html>
   <html lang="en">
   <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Hello World</title>
-          <!-- Include Bootstrap 5 CSS -->
-          <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
-          <!-- Include MDBootstrap CSS -->
-          <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.20.0/js/mdb.min.js" rel="stylesheet">
-          <!-- Include FontAwesome CSS -->
-          <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <!-- Include Bootstrap 5 CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Include MDBootstrap CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.20.0/js/mdb.min.js" rel="stylesheet">
+    <!-- Include FontAwesome CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet">
   </head>
   <body>
-         {{ .Body }}
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"></script>
+    {{ .Body }}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"></script>
   </body>
   </html>
 `
@@ -43,41 +43,42 @@ const Editor = `
   <!DOCTYPE html>
   <html lang="en">
   <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>WYSIWYG Editor Example</title>
-          <!-- Include CKEditor -->
-          <script src="https://cdn.ckeditor.com/ckeditor5/37.0.0/classic/ckeditor.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WYSIWYG Editor Example</title>
+    <!-- Include CKEditor -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/37.0.0/classic/ckeditor.js"></script>
   </head>                                                                                                                                                                                      <body>
-                                                                                                                        <h1>WYSIWYG Editor Example</h1>
-                                             <form action="/submit" method="POST">
-                  <textarea id="editor" name="content"></textarea>
-                  <button type="submit">Submit</button>
-          </form>
-                                                                                                                            <script>
-                                                     // Initialize CKEditor with source editing enabled
-                  ClassicEditor
-                          .create(document.querySelector('#editor'), {
-                                  toolbar: ['', 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|', 'indent', 'outdent', '|', 'blockQuote', 'insertTable', '|', 'undo', 'redo', '|', 'source', 'uploadImage', 'blockQuote', 'codeBlock'],
-  /*
-  toolbar: {
-          items: [
-                  'undo', 'redo',
-                  '|', 'heading',
-                  '|', 'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
-                  '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
-                  '|', 'link', 'uploadImage', 'blockQuote', 'codeBlock',
-                  '|', 'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent'
-          ],
-          shouldNotGroupWhenFull: false
-  }
-  */
-                                  language: 'en'
-                          })
-                          .catch(error => {
-                                  console.error(error);
-                          });
-          </script>                                                                                                                                                                                </body>
+  <h1>WYSIWYG Editor Example</h1>
+  <form action="/submit" method="POST">
+    <textarea id="editor" name="content"></textarea>
+    <button type="submit">Submit</button>
+  </form>
+  <script>
+  // Initialize CKEditor with source editing enabled
+  ClassicEditor
+    .create(document.querySelector('#editor'), {
+      toolbar: ['', 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|', 'indent', 'outdent', '|', 'blockQuote', 'insertTable', '|', 'undo', 'redo', '|', 'source', 'uploadImage', 'blockQuote', 'codeBlock'],
+	/*
+	  toolbar: {
+		items: [
+		  'undo', 'redo',
+		  '|', 'heading',
+		  '|', 'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
+		  '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
+		  '|', 'link', 'uploadImage', 'blockQuote', 'codeBlock',
+		  '|', 'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent'
+		],
+		shouldNotGroupWhenFull: false
+	  }
+    */
+    language: 'en'
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  </script>
+  </body>
   </html>
 `
 
